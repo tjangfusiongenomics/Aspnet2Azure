293aa017b4948f67110a8f68b055ef74c660275e
tjang <tjang@fusiongenomics.com>
2017-01-29 08:41:25 -0800

    first commit

---
+++ lab2Application/lab2Application/Controllers/FileController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab2Application.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            string[] files = Directory.GetFiles(Server.MapPath("~/TextFiles"));
+
+            for(int i=0; i<files.Length; i++)
+            {
+                int last_slash = files[i].Split('\\').Length - 1;
+                files[i] = files[i].Split('\\')[last_slash];
+            }
+            ViewBag.files = files;
+            return View();
+        }
+        public ActionResult Display(string id)
+        {
+            string path = Server.MapPath("~/TextFiles/"+id);
+            StreamReader reader = new StreamReader(path);
+            ViewBag.content = reader.ReadLine();
+
+            return View();
+        }
+    }
+}
---
+++ lab2Application/lab2Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab2Application.Startup))]
+namespace lab2Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
